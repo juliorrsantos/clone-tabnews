6214586e4eba1d3ace64b3741aeb7c9df6d02352
Julio R R Santos <juliorrsantos@hotmail.com>
2025-03-26 10:52:15 -0300

    refactor module

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,40 +3,43 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+
+  let dbClient;
 
   try {
-    switch (request.method) {
-      case "GET":
-        const pendingMigrations = await migrationRunner(
-          defaultMigrationOptions,
-        );
-        response.status(200).json(pendingMigrations);
-        break;
+    dbClient = await database.getNewClient();
 
-      case "POST":
-        const migratedMigrations = await migrationRunner({
-          ...defaultMigrationOptions,
-          dryRun: false,
-        });
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-        if (migratedMigrations.length > 0) {
-          response.status(201).json(migratedMigrations);
-        } else {
-          response.status(200).json(migratedMigrations);
-        }
-        break;
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return response.status(200).json(pendingMigrations);
+    }
 
-      default:
-        response.status(405).end();
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
     }
   } catch (error) {
     console.error(error);
