aea246c06647cac45a5016a6aab64743bcb9ffc1
Julio <juliorrsantos@hotmail.com>
2025-04-18 17:22:05 -0300

    test: rename all descriptions to use new pattern

--- tests/Integration/api/v1/migrations/delete.test.js
+++ tests/Integration/api/v1/migrations/delete.test.js
@@ -6,10 +6,14 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("DELETE to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
-  });
+describe("DELETE /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Trying to delete migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
 
-  expect(response.status).toBe(405);
+      expect(response.status).toBe(405);
+    });
+  });
 });
