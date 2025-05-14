5f1dafceb273b9febf9bc828e604b26673d09803
Julio R R Santos <juliorrsantos@hotmail.com>
2025-05-14 14:33:02 -0300

    feat: add `PATCH` support on `/api/v1/users/[username]`

--- tests/Integration/api/v1/users/post.test.js
+++ tests/Integration/api/v1/users/post.test.js
@@ -87,8 +87,8 @@ describe("POST /api/v1/users", () => {
       const response2Body = await response2.json();
       expect(response2Body).toEqual({
         name: "ValidationError",
-        message: "O email informado já está sendo utilizado",
-        action: "Utilize outro email para realizar o cadastro",
+        message: "O email informado já está sendo utilizado.",
+        action: "Utilize outro email para realizar esta operação.",
         status_code: 400,
       });
     });
@@ -126,7 +126,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O username informado já está sendo utilizado",
-        action: "Utilize outro username para realizar o cadastro",
+        action: "Utilize outro username para realizar esta operação.",
         status_code: 400,
       });
     });
